1afd05840a0453755820c351af9e593d46adf38c
Potapov Dmitriy <potapovd3012@gmail.com>
2021-07-14 01:47:42 -0700

    add custom level

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,4 +1,30 @@
 /* eslint-disable no-console */
+const collors = {
+  Reset: '\x1b[0m',
+  Bright: '\x1b[1m',
+  Dim: '\x1b[2m',
+  Underscore: '\x1b[4m',
+  Blink: '\x1b[5m',
+  Reverse: '\x1b[7m',
+  Hidden: '\x1b[8m',
+  FgBlack: '\x1b[30m',
+  FgRed: '\x1b[31m',
+  FgGreen: '\x1b[32m',
+  FgYellow: '\x1b[33m',
+  FgBlue: '\x1b[34m',
+  FgMagenta: '\x1b[35m',
+  FgCyan: '\x1b[36m',
+  FgWhite: '\x1b[37m',
+  BgBlack: '\x1b[40m',
+  BgRed: '\x1b[41m',
+  BgGreen: '\x1b[42m',
+  BgYellow: '\x1b[43m',
+  BgBlue: '\x1b[44m',
+  BgMagenta: '\x1b[45m',
+  BgCyan: '\x1b[46m',
+  BgWhite: '\x1b[47m',
+};
+
 function wrapInGreen(txt: string): string {
   return `\u001b[34m${txt}\u001b[0m`;
 }
@@ -53,11 +79,16 @@ const logger = {
   },
   setLogLevel(level) {
     this.logLevel = level;
+  },
+  addCustomLevel(loggerDescription, consoleOutput: string, description: string, color = 'FgWhite') {
+    logger[loggerDescription] = (...args) => {
+      console[consoleOutput](`${colors[color]}${description}${collors.Reset}`, ...args);
+    };
   }
 };
 
 
-function setLogLevel(level: 'ERROR' | 'WARN' | 'INFO' | 'VERBOSE' | 'MUTE') {
+function setLogLevel(level: 'ERROR' | 'WARN' | 'INFO' | 'VERBOSE' | 'MUTE' | string) {
   logger.setLogLevel(level);
 }
 
